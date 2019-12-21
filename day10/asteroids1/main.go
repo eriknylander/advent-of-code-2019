63402ee0f50cfb7a57e015a2d8f408acd60b3c2f
Erik Nylander <nylander.e@gmail.com>
2019-12-21 16:32:00 +0100

    Day 10 part 1 working

--- day10/asteroids1/main.go
+++ day10/asteroids1/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -25,36 +26,86 @@ func buildSystem(path string) [][]string {
 }
 
 type asteroid struct {
-	x          int
-	y          int
-	observable int
+	x                int
+	y                int
+	asteroidsInSight int
 }
 
 func main() {
-	system := buildSystem("./example1.txt")
-	// potentialAsteroids := []asteroid{}
-	// for y := 0; y < len(system); y++ {
-	// 	for x := 0; x < len(system[y]); x++ {
-	// 		if system[y][x] == "#" {
-	// 			potentialAsteroids = append(potentialAsteroids, asteroid{
-	// 				x:          x,
-	// 				y:          y,
-	// 				observable: asteroidsInSight(x, y, system),
-	// 			})
-	// 		}
-	// 	}
-	// }
+	system := buildSystem("./input.txt")
+	asteroidsInSystem := []asteroid{}
 
-	a := asteroidsInSight(4, 2, system)
-	fmt.Println(a)
+	for y := 0; y < len(system); y++ {
+		for x := 0; x < len(system[y]); x++ {
+			if system[y][x] == "#" {
+				asteroidsInSystem = append(asteroidsInSystem, asteroid{
+					x: x,
+					y: y,
+				})
+			}
+		}
+	}
+
+	for i, asteroid := range asteroidsInSystem {
+		diag := lookDiagonal(asteroid, asteroidsInSystem)
+		hv := lookHorisontalAndVertical(asteroid, system)
+		fmt.Printf("x: %d, y: %d, observable: diag: %d, hv: %d\n", asteroid.x, asteroid.y, diag, hv)
+		asteroidsInSystem[i].asteroidsInSight = diag + hv
+	}
+
+	sort.Slice(asteroidsInSystem, func(i, j int) bool {
+		return asteroidsInSystem[i].asteroidsInSight > asteroidsInSystem[j].asteroidsInSight
+	})
+
+	best := asteroidsInSystem[0]
+	fmt.Printf("%d,%d, %d\n", best.x, best.y, best.asteroidsInSight)
 }
 
-func asteroidsInSight(x, y int, system [][]string) int {
+func lookDiagonal(a asteroid, asteroids []asteroid) int {
+	type relation struct {
+		x bool
+		y bool
+		k float64
+	}
+	lineMap := map[relation]bool{}
+	for _, o := range asteroids {
+		if a.x == o.x && a.y == o.y {
+			continue
+		}
+
+		dx := float64(a.x - o.x)
+		if dx == 0 {
+			continue
+		}
+
+		dy := float64(a.y - o.y)
+		if dy == 0 {
+			continue
+		}
+
+		k := dy / dx
+		key := relation{k: k}
+		if dx > 0 {
+			key.x = true
+		}
+
+		if dy > 0 {
+			key.y = true
+		}
+
+		if _, exists := lineMap[key]; !exists {
+			lineMap[key] = true
+		}
+	}
+	return len(lineMap)
+}
+
+func lookHorisontalAndVertical(a asteroid, system [][]string) int {
 	inSight := 0
-	inSight += lookRight(x, y, system)
-	inSight += lookLeft(x, y, system)
-	inSight += lookUp(x, y, system)
-	inSight += lookDown(x, y, system)
+	inSight += lookRight(a.x, a.y, system)
+	inSight += lookLeft(a.x, a.y, system)
+	inSight += lookUp(a.x, a.y, system)
+	inSight += lookDown(a.x, a.y, system)
 	return inSight
 }
 
