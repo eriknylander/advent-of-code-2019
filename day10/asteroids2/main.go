c21f7ad92301d0636925a0412f3a66e277fd94fd
Erik Nylander <nylander.e@gmail.com>
2019-12-23 16:29:15 +0100

    Finally got it, angles, distances and laps ftw

--- day10/asteroids2/main.go
+++ day10/asteroids2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -26,9 +27,14 @@ func buildSystem(path string) [][]string {
 }
 
 type asteroid struct {
-	x                int
-	y                int
-	asteroidsInSight []asteroid
+	x int
+	y int
+}
+
+type relatedAsteroid struct {
+	a        asteroid
+	distance float64
+	angle    float64
 }
 
 func main() {
@@ -46,11 +52,66 @@ func main() {
 		}
 	}
 
-	for _, a := range asteroidsInSystem {
-		for _, b := range asteroidsInSystem {
-			angle, distance := getAngleAndDistance(a, b)
-		}
+	type candidate struct {
+		asteroid  asteroid
+		theOthers []relatedAsteroid
+		inSight   int
 	}
+
+	candiates := []candidate{}
+
+	for _, a := range asteroidsInSystem {
+		others := []relatedAsteroid{}
+		otherMap := map[float64]bool{}
+		for _, b := range asteroidsInSystem {
+			if a.x == b.x && a.y == b.y {
+				continue
+			}
+			angle, distance := getAngleAndDistance(a, b)
+			others = append(others, relatedAsteroid{a: b, distance: distance, angle: angle})
+			otherMap[angle] = true
+		}
+		sort.SliceStable(others, func(i, j int) bool {
+			iAst := others[i]
+			jAst := others[j]
+			if iAst.angle == jAst.angle {
+				return iAst.distance < jAst.distance
+			}
+
+			return iAst.angle < jAst.angle
+		})
+
+		candiates = append(candiates, candidate{asteroid: a, theOthers: others, inSight: len(otherMap)})
+	}
+
+	sort.SliceStable(candiates, func(i, j int) bool {
+		return candiates[i].inSight > candiates[j].inSight
+	})
+	best := candiates[0]
+
+	lastAngle := -1.0
+	lap := 0
+	laps := make([][]relatedAsteroid, len(best.theOthers))
+	for _, a := range best.theOthers {
+		if a.angle != lastAngle {
+			if lap != 0 {
+				lap = 0
+			}
+		} else {
+			lap++
+		}
+
+		laps[lap] = append(laps[lap], a)
+		lastAngle = a.angle
+	}
+
+	all := []relatedAsteroid{}
+	for _, l := range laps {
+		all = append(all, l...)
+	}
+	twoHundreth := all[199].a
+	fmt.Printf("x: %d, y: %d, asteroids in sight: %d\n", best.asteroid.x, best.asteroid.y, best.inSight)
+	fmt.Printf("200th vaporized: x:%d, y:%d, answer: %d\n", twoHundreth.x, twoHundreth.y, (100*twoHundreth.x)+twoHundreth.y)
 }
 
 func getAngleAndDistance(a, b asteroid) (float64, float64) {
